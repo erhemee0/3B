d36b30ea873cfb559d95c7a7fb11cdd8ee95e75c
erhemee0 <erhmeealtan@gmail.com>
2024-02-06 08:06:06 +0800

    AI lab1 zasaw

--- AI/lab1/jishee1.py
+++ AI/lab1/jishee1.py
@@ -6,9 +6,4 @@ day_2 = ['home','school','job','home']
 
 days = day_1 + day_2
 
-unique_locations = set(days)
-for location in unique_locations:
-    a = days.count(location)
-    print(location,a)
-
-# print(collections.Counter(days))
+print(collections.Counter(days))
